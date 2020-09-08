80f38b1cf1ab9054e2c8b2fd9b412dd6b9d2a1ac
mWuMadeira <70650952+mWuMadeira@users.noreply.github.com>
2020-09-08 11:48:18 -0400

    Update Main.java

--- testModule/src/Main.java
+++ testModule/src/Main.java
@@ -1,22 +1,5 @@
-/**
- * Author: Mary Wu
- * Date: 9/8/2020
- **/
 public class Main {
     public static void main(String[] args) {
-        String myString1 = "Hello World";
-        System.out.println(myString1);
-        myString1 = "Hello World 1";
-        System.out.println(myString1);
-
-        String myString2 = "I'm Mary Wu";
-        System.out.println(myString2);
-        myString2 = "I'm Mary Wu 1'";
-        System.out.println(myString2);
-
-        String myString3 = "Buh Bye World";
-        System.out.println(myString3);
-        myString3 = "Buh Bye World 1";
-        System.out.println(myString3);
+        
     }
 }
